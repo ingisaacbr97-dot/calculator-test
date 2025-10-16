3d2923c94b3aaeb0e28db7df1b26539224aa217e
Isaac <ingisaacbr97@gmail.com>
2025-10-16 11:06:08 -0600

    Ejercicio de Tests para Calculadora Java Terminados

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,16 @@
 public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
 
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
 }
